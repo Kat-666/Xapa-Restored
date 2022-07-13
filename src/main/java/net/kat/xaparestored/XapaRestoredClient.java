2a978879ec4001ce7124e28c085e800451b9ffd3
Kat-666 <kakatsuiki.uzariki@gmail.com>
2022-07-12 20:52:57 -0600

    test

---
+++ src/main/java/net/kat/xaparestored/XapaRestoredClient.java
@@ -0,0 +1,10 @@
+package net.kat.xaparestored;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class XapaRestoredClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/kat/xaparestored/XapaRestoredMod.java
@@ -0,0 +1,14 @@
+package net.kat.xaparestored;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class XapaRestoredMod implements ModInitializer {
+	public static final String MOD_ID = "xaparestored";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
